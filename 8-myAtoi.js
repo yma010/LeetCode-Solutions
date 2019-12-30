9b79ca821da2781b8fac4fe6bce943f4e8c87e1a
yma010 <yma010@ucr.edu>
2019-12-29 16:22:00 -0800

    myAtoi

--- 8-myAtoi.js
+++ 8-myAtoi.js
@@ -13,16 +13,16 @@
 // Only the space character ' ' is considered as whitespace character.
 // Assume we are dealing with an environment which could only store integers within the 32-bit signed integer range: [−231,  231 − 1]. 
 // If the numerical value is out of the range of representable values, INT_MAX (231 − 1) or INT_MIN (−231) is returned.
-
+//0x7FFFFFFF 
 function myAtoi(str) {
-  
-
+  let int = parseInt(str);
+  return int > 0x7FFFFFFF ? 0x7FFFFFFF : int < -0x7FFFFFFF ? -0x7FFFFFFF : isNaN(int) ? 0 : int;
 }
 
-str = '42' // 42
+// str = '42' // 42
 // str = "   -42" // -42
 // str = "4193 with words" // 4193
-// str = "words and 987" // 0
+str = "words and 987" // 0
 // str = "-91283472332" // -2147483648
 
 console.log(myAtoi(str));
